feac8663b5641774319a352383b73be9ee2341ab
strout18 <stellatrout@mac.com>
2021-09-18 15:50:17 -0700

    cleaned makefile, refactored

--- Makefile
+++ Makefile
@@ -3,18 +3,9 @@ CXXFLAGS=-Wall -Wextra -pedantic -Werror -std=c++17 -O0 -g
 LDFLAGS=$(CXXFLAGS)
 OBJ=$(SRC:.cc=.o)
 
-all:  test_bitio test_hforest test_huffman test_tree encoder decoder
+all:  test_bitio encoder decoder
 
 test_bitio: bitio.o test_bitio.o
-	$(CXX) $(LDFLAGS) -o $@ $^
-
-test_tree: test_tree.o tree.o
-	$(CXX) $(LDFLAGS) -o $@ $^
-
-test_huffman: test_huffman.o huffman.o hforest.o
-	$(CXX) $(LDFLAGS) -o $@ $^
-
-test_hforest: test_hforest.o hforest.o
 	$(CXX) $(LDFLAGS) -o $@ $^
 
 encoder: encoder.o bitio.o
@@ -30,7 +21,4 @@ clean:
 	rm -rf *.o test_bitio encoder decoder
 
 test: all
-	./test_tree
-	./test_hforest
-	./test_huffman
 	./test_bitio
--- test_bitio.cc
+++ test_bitio.cc
@@ -14,12 +14,12 @@ test_1_bit()
   std::stringstream bits;
   {
     BitIO bitio(&bits, nullptr);
-    bitio.output_bit(1);
+    bitio.write_bit(1);
   }
   {
     BitIO bitio(nullptr, &bits);
-    assert(bitio.input_bit() == true);
-    assert(bitio.input_bit() == false);  // Should just be a trailing zero
+    assert(bitio.read_bit() == true);
+    assert(bitio.read_bit() == false);  // Should just be a trailing zero
   }
 }
 
@@ -31,18 +31,18 @@ test_4_bits()
   std::stringstream bits;
   {
     BitIO bitio(&bits, nullptr);
-    bitio.output_bit(0);
-    bitio.output_bit(1);
-    bitio.output_bit(0);
-    bitio.output_bit(1);
+    bitio.write_bit(0);
+    bitio.write_bit(1);
+    bitio.write_bit(0);
+    bitio.write_bit(1);
   }
   {
     BitIO bitio(nullptr, &bits);
-    assert(bitio.input_bit() == false);
-    assert(bitio.input_bit() == true);
-    assert(bitio.input_bit() == false);
-    assert(bitio.input_bit() == true);
-    assert(bitio.input_bit() == false);  // Should just be a trailing zero
+    assert(bitio.read_bit() == false);
+    assert(bitio.read_bit() == true);
+    assert(bitio.read_bit() == false);
+    assert(bitio.read_bit() == true);
+    assert(bitio.read_bit() == false);  // Should just be a trailing zero
   }
 }
 
@@ -55,23 +55,23 @@ test_8_bits()
     BitIO bitio0(&bits0, nullptr);
     BitIO bitio1(&bits1, nullptr);
     for (int i = 0; i < 8; ++i) {
-      bitio0.output_bit(0);
+      bitio0.write_bit(0);
     }
     for (int i = 0; i < 8; ++i) {
-      bitio1.output_bit(1);
+      bitio1.write_bit(1);
     }
   }
   {
     BitIO bitio0(nullptr, &bits0);
     BitIO bitio1(nullptr, &bits1);
     for (int i = 0; i < 8; ++i) {
-      assert(!bitio0.input_bit());
+      assert(!bitio0.read_bit());
     }
-    bitio0.input_bit();
+    bitio0.read_bit();
     assert(bits0.eof()); // Should be no trailing zeros this time!
 
     for (int i = 0; i < 8; ++i) {
-      assert(bitio1.input_bit());
+      assert(bitio1.read_bit());
     }
   }
 }
@@ -84,15 +84,15 @@ test_9_bits()
   {
     BitIO bitio1(&bits1, nullptr);
     for (int i = 0; i < 9; ++i) {
-      bitio1.output_bit(1);
+      bitio1.write_bit(1);
     }
   }
   {
     BitIO bitio1(nullptr, &bits1);
     for (int i = 0; i < 9; ++i) {
-      assert(bitio1.input_bit());
+      assert(bitio1.read_bit());
     }
-    assert(!bitio1.input_bit());
+    assert(!bitio1.read_bit());
   }
 }
 
@@ -104,15 +104,15 @@ test_16_bits()
   {
     BitIO bitio(&bits, nullptr);
     for (int i = 0; i < 16; ++i) {
-      bitio.output_bit(i % 2);
+      bitio.write_bit(i % 2);
     }
   }
   {
     BitIO bitio(nullptr, &bits);
     for (int i = 0; i < 16; ++i) {
-      assert(bitio.input_bit() == i % 2);
+      assert(bitio.read_bit() == i % 2);
     }
-    bitio.input_bit();
+    bitio.read_bit();
     assert(bits.eof()); // Should be no trailing zeros this time!
   }
 }
@@ -125,15 +125,15 @@ test_100_bits()
   {
     BitIO bitio(&bits, nullptr);
     for (int i = 0; i < 100; ++i) {
-      bitio.output_bit(!(i % 2));
+      bitio.write_bit(!(i % 2));
     }
   }
   {
     BitIO bitio(nullptr, &bits);
     for (int i = 0; i < 100; ++i) {
-      assert(bitio.input_bit() != i % 2);
+      assert(bitio.read_bit() != i % 2);
     }
-    assert(!bitio.input_bit());
+    assert(!bitio.read_bit());
   }
 }
 
