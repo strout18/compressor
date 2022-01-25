09f5c6d86612232977e38bfbf3f431f0bb23160e
strout18 <stellatrout@mac.com>
2022-01-24 20:35:23 -0800

    changes

--- pipeline/window.py
+++ pipeline/window.py
@@ -15,7 +15,9 @@ def slice_window(winlen, tkarr, index, tkzer):
     # if index == 0:
     #     return txtarr[0]
     if index < winlen:  # slice from start of line til word if line (up til word) is shorter than window
+        print ("slicing from " + str(index))
         return tkzer.convert_tokens_to_string(tkarr[:index])
     else:
         start = index - winlen
+        print ("slicing from " + str(start) + " to " + str(index))
         return tkzer.convert_tokens_to_string(tkarr[start:index])
