f5361e90943e72305b3e2cd1bc35706303be0b5c
strout18 <stellatrout@mac.com>
2022-01-24 20:27:27 -0800

    changes

--- pipeline/pipelinepredicter.py
+++ pipeline/pipelinepredicter.py
@@ -5,22 +5,23 @@ modelname = "roberta-large"
 tkzer = utils.get_tkzer(modelname)
 unmasker = pipeline('fill-mask', model=modelname)
 with open('roberta-large_med_w8.txt', 'a') as f:
-    for x in range(7, 5006):
+    for x in range(2, 5002):
         if (x % 50 == 0):
             print (str(x))
         # if (len(words) > 125):
         #     words = words[-124:]    # to cut down runtime (8 wds with spaces probably less than 125 chars)
         split = tkzer.tokenize(words)
+        print ("tokens are " + str(split))
         # print(str(split))
         txtrange = utils.slice_window(8, split, x, tkzer)
         # txtrange = ics.slice_window(8, split, x)
         print ("Calling with prev " + txtrange)
         guess = unmasker(txtrange + f"{tkzer.mask_token}")[0]["token_str"]
         print ("guess " + guess)
-        print (str(guess.isalpha()))
-        if not guess.isalpha() and guess != ".":
-            guess = "The"
-            print ("here")
+        # print (str(guess.isalpha()))
+        # if not guess.isalpha() and guess != ".":
+        #     guess = "The"
+        #     print ("here")
         words += guess
         # print ("words " + words)
         f.write(guess)
