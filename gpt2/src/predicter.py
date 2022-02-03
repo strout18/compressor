dbc46a459fe0597b5ae92f51a54a53232cab815e
strout18 <stellatrout@mac.com>
2022-02-02 22:16:37 -0800

    moves" q "

--- gpt2/src/predicter.py
+++ gpt2/src/predicter.py
@@ -1,19 +1,38 @@
-import mainencoderchar
+import gpt_encoder
 import ics_api as ics
-words = "The dog"
-with open('gpt_med_w8.txt', 'a') as f:
-    for x in range(1, 5000):
-        if (x % 50 == 0):
-            print (str(x))
-        # if (len(words) > 125):
-        #     words = words[-124:]    # to cut down runtime (8 wds with spaces probably less than 125 chars)
-        split = mainencoderchar.cleansplit(words)
-        # print(str(split))
-        txtrange = ics.slice_window(8, split, x)
-        # print ("Calling with prev " + txtrange)
-        newwd = ics.run_model(txtrange, length=1, top_k=40)
-        # print ("New wd " + newwd)
-        words += newwd
-        # print ("words " + words)
-        f.write(newwd)
-    # print (words)
+from itertools import cycle
+# words = "The dog"
+seeds = ["The dog", "The carpenter saw the fog", "So the walrus said", "On a balcony", "Waterfalls never", "But how can that be possible"]
+seedpool = cycle(seeds)
+with open('gpt_med_w8_v2.txt', 'a') as f:
+    for y in range(0, 15):
+        words = next(seedpool)
+        for x in range(1, 350):     # guessing this is a good number to stop the train from going off the rails
+            if (x % 50 == 0):
+                print (str(x))
+            split = gpt_encoder.cleansplit(words)
+            # print(str(split))
+            txtrange = ics.slice_window(8, split, x)
+            # print ("Calling with prev " + txtrange)
+            newwd = ics.run_model(txtrange, length=1, top_k=40)
+            # print ("New wd " + newwd)
+            words += newwd
+            # print ("words " + words)
+            f.write(newwd)
+    
+        
+    # for x in range(1, 5000):
+    #     if (x % 50 == 0):
+    #         print (str(x))
+    #     # if (len(words) > 125):
+    #     #     words = words[-124:]    # to cut down runtime (8 wds with spaces probably less than 125 chars)
+    #     split = gpt_encoder.cleansplit(words)
+    #     # print(str(split))
+    #     txtrange = ics.slice_window(8, split, x)
+    #     # print ("Calling with prev " + txtrange)
+    #     newwd = ics.run_model(txtrange, length=1, top_k=40)
+    #     # print ("New wd " + newwd)
+    #     words += newwd
+    #     # print ("words " + words)
+    #     f.write(newwd)
+    # # print (words)
