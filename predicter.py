bf65d5d0ffb4171d1e075a96bbc07306cc305c90
strout18 <stellatrout@mac.com>
2022-02-06 20:03:11 -0800

    moves

--- predicter.py
+++ predicter.py
@@ -2,12 +2,14 @@ import gpt2.src.gpt_encoder as enc
 import gpt2.src.ics_api as ics
 from itertools import cycle
 # words = "The dog"
-seeds = ["The dog", "The carpenter saw the fog", "So the walrus said", "On a balcony", "Waterfalls never", "But how can that be possible"]
+seeds = [" The dog", " The carpenter saw the fog", " So the walrus said", " On a balcony", " Waterfalls never", " But how can that be possible"]
 seedpool = cycle(seeds)
-with open('gpt_med_w8_v2.txt', 'a') as f:
+words = ""
+with open('gpt_med_w8_v3.txt', 'a') as f:
     for y in range(0, 15):
-        words = next(seedpool)
-        f.write(words)
+        newseed = next(seedpool)
+        words += newseed
+        f.write(newseed)
         for x in range(1, 350):     # guessing this is a good number to stop the train from going off the rails
             if (x % 50 == 0):
                 print (str(x))
