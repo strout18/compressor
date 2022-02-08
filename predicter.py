80ee486ddbfebedb1d535e6ceb19cf95a64c79e9
strout18 <stellatrout@mac.com>
2022-02-08 13:51:07 -0800

    moves

--- predicter.py
+++ predicter.py
@@ -4,25 +4,43 @@ from itertools import cycle
 # words = "The dog"
 seeds = ["The dog", "The carpenter saw the fog", "So the walrus said", "On a balcony", 
     "Waterfalls never", "But how can that be possible", "The cat", "You would do it too",
-    "Elect me president and", "Should you even be aware", "economy rose by twelve",
-    "cry me a river because", "whatever because he's in love!", "The lamp sat creatively",
+    "Elect me president and", "Should you even be aware", "The economy rose by twelve",
+    "Cry me a river because", "whatever because he's in love!", "The lamp sat creatively",
     "Can you tell I'm bored", "This thesis is taking my serotonin", "Over eleven thousand",
     "Prepare to die", "I am your father", "You're gonna need a bigger", "My wife in a funny voice"
 ]
 # seedpool = cycle(seeds)
 # words = ""
-with open('gpt_med_w8_v3.txt', 'a') as f:
-    for seed in seeds:
+for seed, ct in enumerate(seeds):
+    with open('gpt_med_w8_seed' + str(ct) + ".txt", 'a+') as f:
         words = seed
-        f.write(words)
-        for x in range (1, 238):
+        f.write(seed)
+        for x in range(1, 500):
             if (x % 50 == 0):
-                print(str(x))
+                print (str(x))
+            # if (len(words) > 125):
+            #     words = words[-124:]    # to cut down runtime (8 wds with spaces probably less than 125 chars)
             split = enc.cleansplit(words)
+            # print(str(split))
             txtrange = ics.slice_window(8, split, x)
+            # print ("Calling with prev " + txtrange)
             newwd = ics.run_model(txtrange, length=1, top_k=40)
+            # print ("New wd " + newwd)
             words += newwd
+            # print ("words " + words)
             f.write(newwd)
+# with open('gpt_med_w8_v4.txt', 'a') as f:
+#     for seed in seeds:
+#         words = seed
+#         f.write(words)
+#         for x in range (1, 238):
+#             if (x % 50 == 0):
+#                 print(str(x))
+#             split = enc.cleansplit(words)
+#             txtrange = ics.slice_window(8, split, x)
+#             newwd = ics.run_model(txtrange, length=1, top_k=40)
+#             words += newwd
+#             f.write(newwd)
 
 
     # for y in range(0, 15):
