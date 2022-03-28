1a207e9415dcab663232a32b2c93c626a43ca344
strout18 <stellatrout@mac.com>
2022-03-27 20:19:13 -0700

    moves

---
+++ pipeline/console_altdecoder.py
@@ -0,0 +1,70 @@
+from transformers import pipeline
+import sys
+import intermediateencoding as ie
+import window as utils
+ 
+
+# window = 8
+# TOP_K = 40
+
+def pipeline_decode(argv):
+    # global window, TOP_K
+    total_decoding = ""
+    infile = argv[0]    # file name
+    modelname = argv[1]
+    window = argv[2]
+    # window = int(argv[1])    # do error checking
+    outfile = infile + ".plaintext"  # file name
+    tkzer = utils.get_tkzer(modelname)
+    ftxt = ie.uncrunch_bz2(infile)
+    # with open(infile, 'r') as inf:
+    #     ftxt = inf.read()
+    # print (ftxt)
+    # print ("reading window as " + str(window))
+    numpointer = 0
+    tkpointer = 0
+    mask_token = tkzer.mask_token
+    unmasker = pipeline('fill-mask', model=modelname)
+    encodingsplit = ftxt.find('\n') # division between numbers and incorrect
+    numbers = ftxt[:encodingsplit]
+    incorrect = ftxt[encodingsplit+1:]
+    print ("numbers are ")
+    print (numbers)
+    print ("incorrect")
+    print (incorrect)
+    while numpointer < len(numbers):
+        guess_cutoff = numbers.index(',', numpointer)
+        guessc = numbers[numpointer:guess_cutoff]
+        guesslen = len(guessc)
+        if int(guessc) == 0:
+            print("Guess char was 0")
+            numpointer += guesslen + 1
+            len_cutoff = numbers.index(',', numpointer)
+            lenchars = numbers[numpointer:len_cutoff]
+            print ("lenchars are " + lenchars)
+            txtlen = int(lenchars)    # gives num of chars in current chunk of unguessed text
+            print("txtlen " + str(txtlen))
+            numpointer += len(lenchars) + 1
+            txtchars = incorrect[tkpointer:tkpointer+txtlen]    # chunk of unguessed text
+            print("decoded as " + txtchars)
+            total_decoding += txtchars
+            tkpointer += txtlen
+        else:
+            print ("Guess char was " + guessc)
+            for _ in range(int(guessc)):
+                decoding_arr = tkzer.tokenize(total_decoding)
+                print ("decoding arr " + str(decoding_arr))
+                prev = utils.slice_window(window, decoding_arr, len(decoding_arr), tkzer)
+                print ("Running with prev " + prev)
+                guess = unmasker(prev + f"{mask_token}")[0]["token_str"]
+                print ("Guess is " + guess)
+                total_decoding += guess
+                # print ("Total decoding now " + total_decoding)
+            numpointer += guesslen + 1 # push pointer to next section, comma
+    print (total_decoding)
+    with open(outfile, 'w') as outf:
+        outf.write(total_decoding)
+
+
+if __name__ == "__main__":
+    pipeline_decode(sys.argv[1:])
---
+++ pipeline/console_altencoder.py
@@ -0,0 +1,126 @@
+from transformers import pipeline
+import os, sys, csv
+import intermediateencoding as ie # make sure it's not confused
+import window as utils
+
+# TO DO DECIDE TOP K
+statfile = "altstats.txt"
+TOP_K = 5
+declare_length = True
+
+
+def on_correct(curr_incorrect, numbers):
+    global declare_length
+    # dump the buffer of incorrects into encoding
+    if declare_length:
+        txt = '0,'
+        txt += str(len(curr_incorrect)) + ","
+        print("curr_incorrect is " + curr_incorrect)
+        print ("Length of incorrect is " + str(len(curr_incorrect)))
+        print ("Str with guess and len: " + txt)
+        numbers += txt
+        declare_length = False
+        curr_incorrect = ""
+        # print ("Extending from incorrect. Total encoding is now " + total_encoding)
+    return curr_incorrect, numbers
+
+def on_incorrect(guessct, numbers):
+    # how much to pad to?
+    global declare_length
+    declare_length = True
+    if guessct > 0:
+        numbers += str(guessct) + ","
+        # print ("Total encoding now " + total_encoding)
+        guessct = 0
+    return guessct, numbers
+
+# args = infile, window
+def pipeline_encode(argv):
+    # global total_encoding, incorrect, guessct, compressed, total
+    #reset globals
+    # global window, TOP_K
+    numbers = ""
+    incorrect = ""
+    guessct = 0
+    compressed = 0
+    total = 0
+    curr_incorrect = ""
+
+    infile = argv[0]    # file name 
+    modelname = argv[1]
+    window = argv[2]
+    # should prob error check command line args
+    out_intermfile = infile + ".interm"  # file name
+    unmasker = pipeline('fill-mask', model=modelname)
+    with open(infile, 'r') as inf:
+        ftxt = inf.read()
+        #print("Writing window")
+        #print ("Encoding now " + total_encoding)
+        tkzer = utils.get_tkzer(modelname)
+        splat = tkzer.tokenize(ftxt)
+        # print(splat)
+        for ct, tk in enumerate(splat):  
+            prev = utils.slice_window(int(window), splat, ct, tkzer) #preceding text
+            # print("#" * 40)
+            # print("Calling extend_encoding on wd \"" + wd + "\" with prev \"" + prev)
+            # global guessct, incorrect, total, compressed
+            # print ("The current word is " + wd + "and the previous word is " + prev)
+            if prev:
+                guess = unmasker(prev + f"{tkzer.mask_token}")[0]["token_str"]
+                # print ("The guess is " + guess)
+            if prev and guess == tkzer.convert_tokens_to_string([tk]):
+                # print ("Guess was correct")
+                guessct += 1
+                compressed += 1
+                # print ("Guesscount " + str(guessct))
+                # print ("Dumping incorrect")
+                # print (incorrect)
+                print("Dumping incorrect, guess was correct")
+                curr_incorrect, numbers = on_correct(curr_incorrect, numbers)  # clear out incorrect buffer before logging correct
+                print ("numbers is now " + numbers)
+            else:
+                print ("Guess was incorrect")
+                # print ("Guess count:" + str(guessct))
+                print ("Dumping correct")
+                guessct, numbers = on_incorrect(guessct, numbers)
+                incorrect += tkzer.convert_tokens_to_string([tk])
+                curr_incorrect += tkzer.convert_tokens_to_string([tk])
+                print ("incorrect is now " + incorrect)
+                total += 1
+            # extend_encoding(wd, prev, guessct, incorrect, total, compressed)  # bitarray
+        #print ("Final dump of correct")
+        curr_incorrect, numbers = on_correct(curr_incorrect, numbers)
+        #print ("Final dump of incorrect")
+        guessct, numbers = on_incorrect(guessct, numbers) # clear buffer after each line
+
+    print("Final encoding")
+    total_encoding = numbers + "\n" + incorrect
+    print(total_encoding)
+    print ("Correct guesses: " + str(compressed))
+    print ("Total guesses: " + str(total))
+    with open(out_intermfile, 'w') as of:  # This step can be removed in future! Only here now to see outputs and stats
+        of.write(total_encoding)
+    ie.crunch_bz2(total_encoding, infile + ".comp")
+    ogsize = os.path.getsize(infile)
+    print ("Size of original file: " + str(ogsize))
+    intermsize = os.path.getsize(infile + ".interm")
+    print ("Size of intermediate file: " + str(intermsize))
+    finalsize = os.path.getsize(infile + ".comp")
+    print ("Size of final file: " + str(finalsize))
+
+    ie.crunch_bz2(ftxt, infile + ".bz2")    # crunch for comparison's sake
+    bzogsize = os.path.getsize(infile + ".bz2")
+    print ("Size of bzipped original file: " + str(bzogsize))
+    print ("final encoding is " + total_encoding)
+
+    # writing stuff to stats
+    # Model,Test,Window,Top_K,Correct Gs,Total Gs,OG size,Interm size,Final size,Bzip OG size
+    # with open(statfile, 'a') as csvfile:
+    #     csvwriter = csv.writer(csvfile)
+    #     csvwriter.writerow([modelname, infile, window, TOP_K, compressed, total, ogsize, intermsize, finalsize, bzogsize])
+
+    
+
+
+if __name__ == "__main__":
+    pipeline_encode(sys.argv[1:])
