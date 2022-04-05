f29a060304e85de5eb52ba051a8bd1b3b3d8dd80
strout18 <stellatrout@mac.com>
2022-04-05 01:04:55 -0700

    moves

---
+++ un_zerochop.py
@@ -0,0 +1,30 @@
+import sys
+def untransform(argv):
+    intermfile = argv[0]
+    transintermfile = argv[1]
+    with open(intermfile, 'r') as inf:
+        ftxt = inf.read()
+        numbers, incorrect = ftxt.split('\n', 1)
+        numpointer = 0
+        newnums = ""
+        curr_inc = True
+        while numpointer < len(numbers):
+            if curr_inc:
+                newnums += "0,"
+                numpointer += 2 # skip 0,
+                len_cutoff = numbers.index(',', numpointer)
+                sliced_inc = numbers[numpointer:len_cutoff + 1]
+                newnums += sliced_inc
+                curr_inc = False
+                numpointer += len(sliced_inc)
+            else:
+                guess_cutoff = numbers.index(',', numpointer)
+                sliced_corr = numbers[numpointer:guess_cutoff + 1]
+                newnums += sliced_corr
+                numpointer += len(sliced_corr)
+                curr_inc = True
+    with open(transintermfile, 'w') as outf:
+        outf.write(newnums + "\n" + incorrect)
+
+if __name__ == "__main__":
+    untransform(sys.argv[1:])
